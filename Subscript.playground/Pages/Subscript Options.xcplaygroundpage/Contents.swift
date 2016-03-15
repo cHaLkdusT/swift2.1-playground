9d1213830ef7bac03ac6fbbc206d2daee0d88d7e
Julius Lundang <jwblundang@gmail.com>
2016-03-16 00:00:58 +0800

    Subscript

---
+++ Subscript.playground/Pages/Subscript Options.xcplaygroundpage/Contents.swift
@@ -0,0 +1,49 @@
+//: Previous[Subscript Usage](@previous)
+//: ## Subscript Options
+/*: 
+* Subscripts can take any number of input parameters and these input parameters can be of any type. 
+* Subscripts can also return any type. 
+* Subscripts can use variable parameters and variadic parameters
+* Subscripts cannot use in-out parameters or provide default parameter values
+
+A class or structure can provide as many subscript implementations as it needs, and the appropriate subscript to be used will be inferred based on the type of the value or values that are contained within the subscript brackets at the point that subscript is used. This definition of multiple subscripts is known as _subscript overloading_.
+While it is most common for subscript to take a single parameter, you can also define a subscript with multiple parameters if it is appropriate for you type.
+*/
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(count: rows * columns, repeatedValue: 0.0)
+    }
+    func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+//: You can construct a new `Matrix` instanc by passing an appropriate row and column to its initializer
+var matrix = Matrix(rows: 2, columns: 2)
+/*: 
+The preceding example creates a new `Matrix` instance with two rows and two columns. The `grid` array for this `Matrix` instance is effectively a flattened version of the matrix, as read from top to lef to bottom right
+![Subscript Matrix](subscriptMatrix01_2x.png "Subscript Matrix")
+Values in the matrix can be set by passing row and column values into the subscript, separated by comma
+*/
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+/*: 
+These two statements call the subscript's setter to set a value of `1.5` in the top right position of the matrix (where `row` is `0` and `column` is `1`) and `3.2` in the bottome left position (where `row` is `1` and the `column` is `0`)
+![Subscript Matrix](subscriptMatrix02_2x.png "Subscript Matrix")
+The `Matrix` subscript's getter and setter both contain an assertion to check that the subscript's `row` and `column` values are valid. To assist with this assertions, `Matrix` includes a convenience method call `indexIsValueForRow(_:column:)`, which checks wheter the requested `row` and `column` are inside the bounds of the matrix.
+An assertion is triggered if you try to access a subscript that is outside of the matrix bounds
+*/
+//matrix[2, 2]
---
+++ Subscript.playground/Pages/Subscript Usage.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: Previous [Subscripts](@previous)
+//: ## Subscript Usage
+/*:
+The exact meaning of "subscript" depends on the context in which it is used. Subscripts are typically used as a shortcut for accessing the member elements in a collection, list, or sequence. You are free to implement subscripts in the most appropriate way for you particular class or structure's functionality.
+For example, Swift's `Dictionary` type implements a subscript to set and retrieve the values stored in a `Dictionary` instance. You can set a value in a dictionary by providing a key of the dictionary's key type within subscript brackets and assigning a value of the dictionary's value type to the subscript
+*/
+var numberOfLegs = ["Spider": 8, "Ant": 6, "Cat": 4]
+numberOfLegs["Bird"] =  2
+//: Swift's `Dictionary` type implements its key-value subscripting as a subscript that takes and returns an _optional_ type. For the `numberOfLegs` dictionary above, the key-value subscript takes and returns a value of type `Int?` or "optional int". The `Dictionary` type uses an optional subscript type to model the fact taht not every key will have a value, and to give a way to delete a value for a key by assigning a `nil` value for that key.
+//: Move on to [Subscript Options](@next)
